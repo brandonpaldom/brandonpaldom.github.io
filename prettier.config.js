1534e479662071f911744f5e50bf4196a9803dfd
Brandon Palmeros <brandonpaldom@gmail.com>
2022-02-20 16:59:55 -0600

    Tailwind CSS installed

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  tailwindConfig: './tailwind.config.js',
+  bracketSpacing: false,
+  singleQuote: true,
+  jsxBracketSameLine: true,
+  trailingComma: 'es5',
+  printWidth: 80,
+};
