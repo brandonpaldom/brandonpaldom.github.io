c7f34ba62efdae2888a434ea7c710c9b88c524f5
Brandon Palmeros <brandonpaldom@gmail.com>
2022-02-20 19:51:35 -0600

    Some options removed

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,4 @@
 module.exports = {
   tailwindConfig: './tailwind.config.js',
-  bracketSpacing: false,
   singleQuote: true,
-  jsxBracketSameLine: true,
-  trailingComma: 'es5',
-  printWidth: 80,
 };
